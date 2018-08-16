59b2ae28d01b55b36f6ad7fc011e7dafc4938cbb
agata okolska <agata.okolska@gmail.com>
2018-08-16 11:35:13 +0200

    install firebase, import firebase, add firebase config

---
+++ src/firebaseConfig.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+
+var config = {
+    apiKey: "AIzaSyBYVLkjf8ZkRq1bBwk1nDLz1ybbnW59DMU",
+    authDomain: "fir-sandbox-65a96.firebaseapp.com",
+    databaseURL: "https://fir-sandbox-65a96.firebaseio.com",
+    projectId: "fir-sandbox-65a96",
+    storageBucket: "fir-sandbox-65a96.appspot.com",
+    messagingSenderId: "183264277584"
+  }
+
+  firebase.initializeApp(config)
+
+  const auth = firebase.auth()
+  const database = firebase.database()
+  const googleProvider = new firebase.auth.GoogleAuthProvider()
