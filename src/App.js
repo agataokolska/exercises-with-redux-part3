ec50a38f96d0d00a7e2f3010b85261b29a2977d7
agata okolska <agata.okolska@gmail.com>
2018-08-16 10:13:15 +0200

    add loading users

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-
+import Users from './views/Users'
 
 class App extends Component {
   render() {
     return (
       <div>
-       
+       <Users />
       </div>
     )
   }
---
+++ src/views/Users.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+const Users = (props) => (
+
+    <div>
+        {
+            props._isUsersAreLoading ?
+                'Users are loading'
+                :
+                props._users.results.map((user, i) => (
+                    <p>{user.name.first} {user.name.last}</p>
+                ))
+        }
+    </div>
+)
+const mapStateToProps = state => ({
+    _users: state.fetchUsers.users,
+    _isUsersAreLoading: state.fetchUsers.isUsersAreLoading
+})
+
+const mapDispatchToProps = dispatch => ({
+
+})
+
+export default connect(mapStateToProps,
+    mapDispatchToProps)(Users)
